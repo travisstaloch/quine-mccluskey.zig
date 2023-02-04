93788ebf147d11470e0166cc3ece0814c513eb9d
Travis Staloch <twostepted@gmail.com>
2023-02-04 12:45:03 -0800

    update build.zig to new API

--- build.zig
+++ build.zig
@@ -1,27 +1,43 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    const mode = b.standardReleaseOptions();
+    const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
 
-    const lib = b.addStaticLibrary("quine-mccluskey", "src/quine-mccluskey.zig");
-    lib.setBuildMode(mode);
+    const lib = b.addStaticLibrary(.{
+        .name = "quine-mccluskey",
+        .root_source_file = .{ .path = "src/quine-mccluskey.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
     lib.install();
 
-    const main_tests = b.addTest("src/tests.zig");
-    main_tests.setBuildMode(mode);
+    const main_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/tests.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
 
-    const simplify_exe = b.addExecutable("simplify", "simplify-c-defines-zig/simplify.zig");
-    simplify_exe.setBuildMode(mode);
+    const simplify_exe = b.addExecutable(.{
+        .name = "simplify",
+        .root_source_file = .{ .path = "simplify-c-defines-zig/simplify.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
     simplify_exe.install();
-    simplify_exe.addPackagePath("quine-mccluskey", "src/quine-mccluskey.zig");
+    simplify_exe.addAnonymousModule(
+        "quine-mccluskey",
+        .{ .source_file = .{ .path = "src/quine-mccluskey.zig" } },
+    );
 
-    const simplify_tests = b.addTest("simplify-c-defines-zig/test_simplify.zig");
-    simplify_tests.setBuildMode(mode);
+    const simplify_tests = b.addTest(.{
+        .root_source_file = .{ .path = "simplify-c-defines-zig/test_simplify.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
     simplify_tests.step.dependOn(b.getInstallStep());
 
     const test_simplify_step = b.step("test-simplify", "Run simplify-c-defines-zig/test_simplify.zig tests\n                               NOTE: you must run `zig build` first otherwise zig-out/bin/simplify is missing");
